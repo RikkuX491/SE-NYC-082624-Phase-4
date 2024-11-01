695d66ce1b020caeb387f1a1d11fab40cdb45c9e
Ricardo Guerra <RikkuG491@gmail.com>
2024-11-01 14:20:26 -0400

    Added solution code for Deliverables

--- server/app.py
+++ server/app.py
@@ -48,21 +48,6 @@ class AllHotels(Resource):
     
 api.add_resource(AllHotels, '/hotels')
 
-# @app.route('/hotels', methods=["GET", "POST"])
-# def all_hotels():
-#     if request.method == 'GET':
-#         hotels = Hotel.query.all()
-#         response_body = [hotel.to_dict(only=('id', 'name')) for hotel in hotels]
-#         return make_response(response_body, 200)
-    
-#     elif request.method == 'POST':
-#         hotel_name = request.json.get('name')
-#         new_hotel = Hotel(name=hotel_name)
-#         db.session.add(new_hotel)
-#         db.session.commit()
-#         response_body = new_hotel.to_dict(rules=('-reviews',))
-#         return make_response(response_body, 201)
-
 # Deliverable # 2 solution code
 class HotelByID(Resource):
     def get(self, id):
@@ -111,34 +96,6 @@ class HotelByID(Resource):
         
 api.add_resource(HotelByID, '/hotels/<int:id>')
 
-# @app.route('/hotels/<int:id>', methods=["GET", "PATCH", "DELETE"])
-# def hotel_by_id(id):
-#     hotel = db.session.get(Hotel, id)
-
-#     if hotel:
-#         if request.method == "GET":
-#             response_body = hotel.to_dict(rules=('-reviews.hotel', '-reviews.customer'))
-#             response_body['customers'] = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in hotel.customers]
-#             return make_response(response_body, 200)
-        
-#         elif request.method == 'PATCH':
-#             for attr in request.json:
-#                 setattr(hotel, attr, request.json.get(attr))
-#             db.session.commit()
-#             response_body = hotel.to_dict(rules=('-reviews',))
-#             return make_response(response_body, 200)
-          
-#         elif request.method == 'DELETE':
-#             db.session.delete(hotel)
-#             db.session.commit()
-#             return make_response({}, 204)
-
-#     else:
-#         response_body = {
-#             "error": "Hotel Not Found"
-#         }
-#         return make_response(response_body, 404)
-
 # Deliverable # 3 solution code
 class AllCustomers(Resource):
     def get(self):
@@ -156,22 +113,6 @@ class AllCustomers(Resource):
         return make_response(response_body, 201)
     
 api.add_resource(AllCustomers, '/customers')
-
-# @app.route('/customers', methods=["GET", "POST"])
-# def all_customers():
-#     if request.method == "GET":
-#         customers = Customer.query.all()
-#         customer_list_with_dictionaries = [customer.to_dict(only=('id', 'first_name', 'last_name')) for customer in customers]
-#         return make_response(customer_list_with_dictionaries, 200)
-    
-#     elif request.method == "POST":
-#         customer_first_name = request.json.get('first_name')
-#         customer_last_name = request.json.get('last_name')
-#         new_customer = Customer(first_name=customer_first_name, last_name=customer_last_name)
-#         db.session.add(new_customer)
-#         db.session.commit()
-#         response_body = new_customer.to_dict(rules=('-reviews',))
-#         return make_response(response_body, 201)
 
 # Deliverable # 4 solution code
 class CustomerByID(Resource):
@@ -221,34 +162,6 @@ class CustomerByID(Resource):
         
 api.add_resource(CustomerByID, '/customers/<int:id>')
 
-# @app.route('/customers/<int:id>', methods=["GET", "PATCH", "DELETE"])
-# def customer_by_id(id):
-#     customer = db.session.get(Customer, id)
-
-#     if customer:
-#         if request.method == "GET":
-#             response_body = customer.to_dict(rules=('-reviews.hotel', '-reviews.customer'))
-#             response_body['hotels'] = [hotel.to_dict(only=('id', 'name')) for hotel in customer.hotels]
-#             return make_response(response_body, 200)
-        
-#         elif request.method == "PATCH":
-#             for attr in request.json:
-#                 setattr(customer, attr, request.json.get(attr))
-#             db.session.commit()
-#             response_body = customer.to_dict(rules=('-reviews',))
-#             return make_response(response_body, 200)
-        
-#         elif request.method == "DELETE":
-#             db.session.delete(customer)
-#             db.session.commit()
-#             return make_response({}, 204)
-        
-#     else:
-#         response_body = {
-#             "error": "Customer Not Found"
-#         }
-#         return make_response(response_body, 404)
-
 # Deliverable # 5 solution code
 class AllReviews(Resource):
     def get(self):
@@ -268,24 +181,6 @@ class AllReviews(Resource):
         return make_response(response_body, 201)
 
 api.add_resource(AllReviews, '/reviews')
-
-# @app.route('/reviews', methods=["GET", "POST"])
-# def all_reviews():
-#     if request.method == "GET":
-#         reviews = Review.query.all()
-#         review_list_with_dictionaries = [review.to_dict(rules=('-hotel.reviews', '-customer.reviews')) for review in reviews]
-#         return make_response(review_list_with_dictionaries, 200)
-    
-#     elif request.method == "POST":
-#         review_rating = request.json.get('rating')
-#         review_text = request.json.get('text')
-#         review_hotel_id = request.json.get('hotel_id')
-#         review_customer_id = request.json.get('customer_id')
-#         new_review = Review(rating=review_rating, text=review_text, hotel_id=review_hotel_id, customer_id=review_customer_id)
-#         db.session.add(new_review)
-#         db.session.commit()
-#         response_body = new_review.to_dict(rules=('-hotel.reviews', '-customer.reviews'))
-#         return make_response(response_body, 201)
 
 # Deliverable # 6 solution code
 class ReviewByID(Resource):
@@ -333,33 +228,6 @@ class ReviewByID(Resource):
             return make_response(response_body, 404)
 
 api.add_resource(ReviewByID, '/reviews/<int:id>')
-
-# @app.route('/reviews/<int:id>', methods=["GET", "PATCH", "DELETE"])
-# def review_by_id(id):
-#     review = db.session.get(Review, id)
-
-#     if review:
-#         if request.method == "GET":
-#             response_body = review.to_dict(rules=('-hotel.reviews', '-customer.reviews'))
-#             return make_response(response_body, 200)
-        
-#         elif request.method == "PATCH":
-#             for attr in request.json:
-#                 setattr(review, attr, request.json.get(attr))
-#             db.session.commit()
-#             response_body = review.to_dict(rules=('-hotel.reviews', '-customer.reviews'))
-#             return make_response(response_body, 200)
-        
-#         elif request.method == "DELETE":
-#             db.session.delete(review)
-#             db.session.commit()
-#             return make_response({}, 204)
-        
-#     else:
-#         response_body = {
-#             "error": "Review Not Found"
-#         }
-#         return make_response(response_body, 404)
 
 if __name__ == "__main__":
     app.run(port=7777, debug=True)
